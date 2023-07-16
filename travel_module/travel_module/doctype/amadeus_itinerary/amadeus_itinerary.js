1674095628f0955cd428f62f873334e045c820d8
UsamaNaveed9 <usamanaveed9263@gmail.com>
2023-07-16 12:34:57 +0000

    App Initalised

---
+++ travel_module/travel_module/doctype/amadeus_itinerary/amadeus_itinerary.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, SMB and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Amadeus Itinerary', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ travel_module/travel_module/doctype/iata_reconciliation/test_iata_reconciliation.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, SMB and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestIATAReconciliation(FrappeTestCase):
+	pass
